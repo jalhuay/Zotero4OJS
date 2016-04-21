0bdc21ef3af0000aaa3e4c88c24884ed874bc636
jalhuay <joelalway@gmail.com>
2016-04-21 10:08:44 -0500

    Added files via upload

---
+++ Zotero4OJS.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+<a style="text-decoration: none;" href="javascript:var d=document,s=d.createElement('script');s.src='https://www.zotero.org/bookmarklet/loader.js';(d.body?d.body:d.documentElement).appendChild(s);void(0);" target="_blank">
+<img src="https://www.zotero.org/support/_media/logo/zotero_512x512x32.png" alt="Guardar en Zotero" width="25" height="25" /> </a>
+
+
